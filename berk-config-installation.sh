3f8cae87942af988e648beb3f31f4c060a0ebc32
Berk <berkbal94@gmail.com>
2022-01-22 21:50:13 +0300

    Configured installation script

--- berk-config-installation.sh
+++ berk-config-installation.sh
@@ -2,6 +2,18 @@
 
 echo "Installing Necessary Packages"
 pacman -Syu
-pacman -S compton python polybar git pavucontrol
+pacman -S compton python git pavucontrol
+
 echo "Polybar theme should be installed manually"
 echo "Polybar Themes: https://github.com/adi1090x/polybar-themes"
+
+# Copy Process
+cp -r alacritty ~/.config/
+cp -r i3 ~/.config/
+
+cp .bash_profile ~/
+cp .bashrc ~/
+cp xinitrc ~/
+
+cp compton.conf ~/.config
+
