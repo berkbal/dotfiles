3e56dbec898432f3c0780ae505dfcb0142a4ef3a
Berk Bal <berkbal94@gmail.com>
2022-10-19 13:26:29 +0300

    stacer'in kuruluma eklenmesi

--- berk-config-installation.sh
+++ berk-config-installation.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Necessary Packages"
 pacman -Syu
-pacman -S compton python git pavucontrol
+pacman -S compton python git pavucontrol thunar alacritty syslog-ng cronie
 
 echo "Polybar theme should be installed manually"
 echo "Polybar Themes: https://github.com/adi1090x/polybar-themes"
@@ -10,6 +10,7 @@ echo "Polybar Themes: https://github.com/adi1090x/polybar-themes"
 # Copy Process
 cp -r alacritty ~/.config/
 cp -r i3 ~/.config/
+cp -r nvim ~/.config/
 
 cp .bash_profile ~/
 cp .bashrc ~/
