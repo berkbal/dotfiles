8ea30937624f96e1f3cc02ee14177ae007db20a6
berk <berk.bal@altosec.com.tr>
2022-10-19 13:33:16 +0300

    Gerekli paketlerin kuruluma eklenmesi

--- berk-config-installation.sh
+++ berk-config-installation.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Necessary Packages"
 pacman -Syu
-pacman -S compton python git pavucontrol thunar alacritty syslog-ng cronie firefox stacer
+pacman -S compton python git pavucontrol nautilus alacritty syslog-ng cronie firefox stacer neofetch
 
 echo "Polybar theme should be installed manually"
 echo "Polybar Themes: https://github.com/adi1090x/polybar-themes"
